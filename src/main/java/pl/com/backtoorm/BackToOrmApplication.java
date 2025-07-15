143389bb72d6fd35649586f9407a97ecaf890403
Przemek.Wolski <Przemyslaw.Wolski@contracted.pmi.com>
2025-07-15 15:18:11 +0200

    Initial commit

---
+++ src/main/java/pl/com/backtoorm/BackToOrmApplication.java
@@ -0,0 +1,13 @@
+package pl.com.backtoorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackToOrmApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BackToOrmApplication.class, args);
+  }
+
+}
---
+++ src/test/java/pl/com/backtoorm/BackToOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.com.backtoorm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackToOrmApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
